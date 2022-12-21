a7593b373a2d21c7dac2ac0ca1b524212c0748d1
Demakova Tatiana <demakova_ts@mail.ru>
2022-12-21 22:22:04 +0700

    update calculator

--- Homework/hw1/src/main/java/org/example/org/hw4/Calculator.java
+++ Homework/hw1/src/main/java/org/example/org/hw4/Calculator.java
@@ -2,61 +2,76 @@ package org.example.org.hw4;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class Calculator {
     private static Logger logger = Logger.getLogger(Calculator.class.getName());
+
     public static void main(String[] args) {
-        startCalculator();
+        LinkedList<Double> operation = new LinkedList<Double>();
+        System.out.println("Простой калькулятор");
+        try (PrintWriter out = new PrintWriter("log-calculator.txt")) {
+            startCalculator(operation, out);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(operation);
     }
 
-//    3. В калькулятор добавьте возможность отменить последнюю операцию.
-    private static void startCalculator(){
-        System.out.println("Простой калькулятор");
+    //    3. В калькулятор добавьте возможность отменить последнюю операцию.
+    private static LinkedList<Double> startCalculator(LinkedList<Double> operation, PrintWriter out) {
         Scanner iScan = new Scanner(System.in);
-        try (PrintWriter out = new PrintWriter("log-calculator.txt")){
+        StringBuilder result = new StringBuilder();
+        System.out.print("Введите оператор (+, -, *, /, cancel): ");
+        String op = iScan.next();
+        out.println("операция: " + op);
+        if (op.contains("cancel")) {
+            operation.removeLast();
+            result.append("отмена последней операции");
+        } else {
             System.out.print("Введите первое число: ");
             double num1 = iScan.nextDouble();
             out.println("значение 1: " + num1);
             System.out.print("Введите второе число: ");
             double num2 = iScan.nextDouble();
             out.println("значение 2: " + num2);
-            System.out.print("Введите оператор (+, -, *, /): ");
-            char op = iScan.next().charAt(0);
-            out.println("операция: " + op);
-
-            iScan.close();
-
-            StringBuilder result = new StringBuilder();
-
-            switch (op){
-                case '+':
+            switch (op) {
+                case "+":
+                    operation.add(num1 + num2);
                     result.append(num1).append(op).append(num2).append("=").append(num1 + num2);
                     break;
-                case '-':
+                case "-":
+                    operation.add(num1 - num2);
                     result.append(num1).append(op).append(num2).append("=").append(num1 - num2);
                     break;
-                case '*':
+                case "*":
+                    operation.add(num1 * num2);
                     result.append(num1).append(op).append(num2).append("=").append(num1 * num2);
                     break;
-                case '/':
-                    if (num2 != 0){
+                case "/":
+                    if (num2 != 0) {
+                        operation.add(num1 / num2);
                         result.append(num1).append(op).append(num2).append("=").append(num1 / num2);
-                    }
-                    else {
+                    } else {
                         result.append("Попытка деления на 0, нельзя делить на 0");
                     }
                     break;
-                default:  result.append("Error!");
+                default:
+                    result.append("Error!");
                     break;
             }
-            System.out.println(result);
-
-            out.println("итог: "+ result);
+        }
+        System.out.println(result);
+        out.println("итог: " + result);
+        String flag = iScan.next();
+        if (!flag.contains("stop")) {
+            return startCalculator(operation, out);
+        } else {
+            iScan.close();
             logger.info("log add");
-        }catch (FileNotFoundException e){
-            throw new RuntimeException(e);
+            return operation;
         }
     }
 }
