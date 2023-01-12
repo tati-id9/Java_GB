a9811434a77c326aa3431ef44253a5819522805f
Demakova Tatiana <demakova_ts@mail.ru>
2023-01-12 16:31:04 +0700

    update task laptop

--- Homework/hw1/src/main/java/org/example/org/hw6/Main.java
+++ Homework/hw1/src/main/java/org/example/org/hw6/Main.java
@@ -1,7 +1,6 @@
 package org.hw6;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     /**
@@ -19,17 +18,18 @@ public class Main {
      * 5. Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
      */
     public static void main(String[] args) {
-        createLaptops();
+        Set<Laptops> setLaptop = createLaptops();
+        task(setLaptop);
     }
 
-    private static void createLaptops(){
+    private static Set<Laptops> createLaptops(){
         Laptops laptop = new Laptops("Honor", "MagicBook X14", "Windows 10 Home", "Intel Core", 8, 256);
         Laptops laptop1 = new Laptops("Honor", "MagicBook X15", "Windows 10 Home", "Intel Core", 8, 256);
         Laptops laptop2 = new Laptops("HP", "15s-eq1142ur", "без ОС", "AMD", 8, 256);
         Laptops laptop3 = new Laptops("HP", "15s-fq3008ur", "без ОС", "Intel Pentium Silver", 8, 256);
         Laptops laptop4 = new Laptops("HUAWEI", "MateBook D 15", "Windows 11 Home", "AMD", 16, 512);
         Laptops laptop5 = new Laptops("ASUS", "VivoBook 15X", "без ОС", "AMD", 16, 512);
-        Laptops laptop6 = new Laptops("Acer", "Nitro 5", "без ОС", "Intel Core", 16, 512);
+        Laptops laptop6 = new Laptops("Acer", "Nitro 5", "без ОС", "Intel Core i3-1005G1 ", 16, 512);
         Laptops laptop7 = new Laptops("Acer", "Aspire 7", "Windows 10 Pro", "AMD", 16, 1000);
 
         Set<Laptops> setLaptop = new HashSet<>();
@@ -41,7 +41,105 @@ public class Main {
         setLaptop.add(laptop5);
         setLaptop.add(laptop6);
         setLaptop.add(laptop7);
-
-        System.out.println(setLaptop);
+        return setLaptop;
     }
+    private static void filterLaptop (Set<Laptops> laptops, String parametr){
+        Set<Laptops> result = new HashSet<>();
+        for (var element: laptops) {
+            String laptop = element.toString();
+            if (laptop.contains(parametr)) {
+                result.add(element);
+            }
+        }
+        if (result.isEmpty()){
+            System.out.println("По заданым критериям ноутбуки не найдены");
+        }
+        else {
+            System.out.println(result);
+        }
+    }
+
+    private static void task(Set<Laptops> laptops){
+        Map<Integer,String> criterion = new HashMap<Integer, String>();
+        criterion.put(1, "Производитель");
+        criterion.put(2, "Операционная система");
+        criterion.put(3, "Производитель процессора");
+        criterion.put(4, "ОЗУ");
+        criterion.put(5, "Объем ЖД");
+
+        System.out.println("Выберите кретерий поиска и введите его номер:");
+        System.out.println(criterion);
+        Scanner iScan = new Scanner(System.in);
+        int numberCriterion = iScan.nextInt();
+
+        switch (numberCriterion){
+            case 1:
+                criterionProducer(laptops);
+                break;
+            case 2:
+                criterionOS(laptops);
+                break;
+            case 3:
+                criterionCUP(laptops);
+                break;
+            default:
+                System.out.println("Вы неверно ввели номер кретерия поиска");
+                break;
+        }
+        iScan.close();
+    }
+
+    private static LinkedHashSet<String> presenceProducer(Set<Laptops> laptops){
+        LinkedHashSet<String> result = new LinkedHashSet<>();
+        for(Laptops lp: laptops){
+            result.add(lp.getProducer());
+        }
+        return result;
+    }
+    private static void criterionProducer(Set<Laptops> laptops){
+        LinkedHashSet<String> prod = presenceProducer(laptops);
+        System.out.println("Введите название одного из доступных производителе " + prod);
+        Scanner iScan = new Scanner(System.in);
+        String str = iScan.next().toString();
+        iScan.close();
+        if (prod.contains(str)){
+            filterLaptop(laptops,str);
+        }
+        else {
+            System.out.println("Вы ввели производителя неверно");
+        }
+    }
+
+    private static void criterionOS(Set<Laptops> laptops){
+        Map<Integer,String> criterion = new HashMap<Integer, String>();
+        criterion.put(1, "без ОС");
+        criterion.put(2, "Windows 11 Home");
+        criterion.put(3, "Windows 11 Pro");
+        criterion.put(4, "Windows 10 Home");
+        criterion.put(5, "Windows 10 Pro");
+        criterion.put(6, "macOS");
+        criterion.put(7, "Linux");
+
+        System.out.println("Выберите кретерий поиска и введите его номер:");
+        System.out.println(criterion);
+        Scanner iScan = new Scanner(System.in);
+        int numberCriterion = iScan.nextInt();
+        iScan.close();
+        filterLaptop(laptops, criterion.get(numberCriterion));
+    }
+
+    private static void criterionCUP(Set<Laptops> laptops){
+        Map<Integer,String> criterion = new HashMap<Integer, String>();
+        criterion.put(1, "AMD");
+        criterion.put(2, "Intel");
+        criterion.put(3, "Apple");
+
+        System.out.println("Выберите кретерий поиска и введите его номер:");
+        System.out.println(criterion);
+        Scanner iScan = new Scanner(System.in);
+        int numberCriterion = iScan.nextInt();
+        iScan.close();
+        filterLaptop(laptops, criterion.get(numberCriterion));
+    }
+
 }
