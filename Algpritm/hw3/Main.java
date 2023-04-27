9780dbb014fca8d6a50dedc0ee03e9ac8e40081f
Demakova Tatiana <demakova_ts@mail.ru>
2023-04-27 10:36:39 +0700

    hw3 algoritm

---
+++ Algpritm/hw3/Main.java
@@ -0,0 +1,17 @@
+package hw3;
+import static hw3.RedBlackTree.printRedBlackTree;
+
+public class Main {
+    public static void main(String[] args) {
+        RedBlackTree tree = new RedBlackTree();
+        tree.add(5);
+        tree.add(7);
+        tree.add(10);
+        tree.add(3);
+        tree.add(6);
+        tree.add(20);
+        tree.add(25);
+        tree.add(35);
+        printRedBlackTree(tree);
+    }
+}
