e8f5d5055d20140833ed2f3d5b08181e92a5d72a
Demakova Tatiana <demakova_ts@mail.ru>
2023-05-01 22:01:16 +0700

    update hw2

---
+++ Algoritms/hw2/List.java
@@ -0,0 +1,28 @@
+package hw2;
+
+public class List {
+    Node head;
+    Node tail;
+
+    public class Node {
+        Node next;
+        Node previous;
+    }
+
+    public void revert(){
+        Node currentNode = head;
+        while (currentNode != null){
+            Node next = currentNode.next;
+            Node previous = currentNode.previous;
+            currentNode.next = previous;
+            currentNode.previous = next;
+            if (previous == null){
+                tail = currentNode;
+            }
+            if (next == null){
+                head = currentNode;
+            }
+            currentNode = next;
+        }
+    }
+}
